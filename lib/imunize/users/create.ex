63501f71ddabc4bf3ddae846bfadbd11a22c3a0e
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-06-26 16:43:53 -0300

    parsing on date receive done

--- lib/imunize/users/create.ex
+++ lib/imunize/users/create.ex
@@ -3,6 +3,7 @@ defmodule Imunize.Users.Create do
 
   def call(params) do
     params
+    |> parse_datetime()
     |> User.changeset()
     |> Repo.insert()
     |> handle_call()
@@ -11,4 +12,21 @@ defmodule Imunize.Users.Create do
   defp handle_call({:ok, %User{} = user}), do: {:ok, user}
 
   defp handle_call({:error, _reason}), do: {:error, "Some error"}
+
+  defp parse_datetime(%{birthday: birthday} = params) do
+    [year, month, day] = String.split(birthday, "-", trim: true)
+
+    naive_date =
+      NaiveDateTime.new!(
+        String.to_integer(year),
+        String.to_integer(month),
+        String.to_integer(day),
+        0,
+        0,
+        0
+      )
+
+    params
+    |> Map.update(:birthday, naive_date, fn _old_value -> naive_date end)
+  end
 end
