c740d33bafb671283c4f2e14591dbbf0acddc036
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-06-26 19:22:58 -0300

    rendering updated

--- lib/imunize_web/views/users_view.ex
+++ lib/imunize_web/views/users_view.ex
@@ -6,10 +6,29 @@ defmodule ImunizeWeb.UsersView do
 
   alias Imunize.User
 
-  def render("create.json", %{user: %User{} = user}) do
+  def render("create.json", %{
+        user:
+          %User{
+            name: name,
+            cep: cep,
+            rg: rg,
+            cpf: cpf,
+            email: email,
+            id: id,
+            birthday: birthday
+          } = _user
+      }) do
     %{
       message: "User created!",
-      user: user
+      user: %{
+        id: id,
+        name: name,
+        cep: cep,
+        rg: rg,
+        cpf: cpf,
+        email: email,
+        birthday: Date.to_string(birthday)
+      }
     }
   end
 end
